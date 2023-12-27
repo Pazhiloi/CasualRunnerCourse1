2039fa8e5e7f3aa48d7e1cf8b2dd458e9d76ce3c
Miroslav Miroslavov <martinsen06@gmail.com>
2023-12-27 14:59:15 +0200

    14 Interacting with the Doors

--- CasRunner1/Assets/CrowdRunner/Scripts/Doors.cs
+++ CasRunner1/Assets/CrowdRunner/Scripts/Doors.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+ public enum BonusType { Addition, Difference, Product, Division }
 public class Doors : MonoBehaviour
 {
 
-  enum BonusType { Addition, Difference, Product, Division }
   [Header("Elements")]
   [SerializeField] private SpriteRenderer rightDoorRenderer;
   [SerializeField] private SpriteRenderer leftDoorRenderer;
   [SerializeField] private TextMeshPro rightDoorText;
   [SerializeField] private TextMeshPro leftDoorText;
+  [SerializeField] private Collider collider;
+
+
 
   [Header("Settings")]
   [SerializeField] private BonusType rightDoorBonusType;
@@ -71,8 +74,27 @@ public class Doors : MonoBehaviour
         leftDoorText.text = "/" + leftDoorBonusAmount;
         break;
     }
-    
-    
+  }
+
+  public int GetBonusAmount(float xPosition){
+    if (xPosition > 0)
+    {
+      return rightDoorBonusAmount;
+    }else{
+      return leftDoorBonusAmount;
+    }
+  }
+  public BonusType GetBonusType(float xPosition){
+    if (xPosition > 0)
+    {
+      return rightDoorBonusType;
+    }else{
+      return leftDoorBonusType;
+    }
+  }
+
+  public void Disable(){
+    collider.enabled = false;
   }
 
 }
---
+++ CasRunner1/Assets/CrowdRunner/Scripts/PlayerDetection.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetection : MonoBehaviour
+{
+
+
+  [Header("Elements")]
+  [SerializeField] private CrowdSystem crowdSystem;
+  Collider[] detectedColliders;
+
+  private void Update()
+  {
+    DetectDoors();
+  }
+  private void DetectDoors()
+  {
+    detectedColliders = Physics.OverlapSphere(transform.position, 1);
+
+    for (int i = 0; i < detectedColliders.Length; i++)
+    {
+      if (detectedColliders[i].TryGetComponent(out Doors doors))
+      {
+        Debug.Log("Hit some doors");
+
+        int bonusAmount = doors.GetBonusAmount(transform.position.x);
+        BonusType bonusType = doors.GetBonusType(transform.position.x);
+
+        doors.Disable();
+
+        crowdSystem.ApplyBonus(bonusType, bonusAmount);
+      }
+    }
+  }
+}
