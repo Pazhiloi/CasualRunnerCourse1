6719510edbe37953eb311f819054f85ba0350261
Miroslav Miroslavov <martinsen06@gmail.com>
2023-12-28 10:55:10 +0200

    24 creating an enemy group
    still have the issue with destroying the enemy prefabs

---
+++ CasRunner1/Assets/CrowdRunner/Scripts/EnemyGroup.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGroup : MonoBehaviour
+{
+  [Header("Elements")]
+  [SerializeField] private Enemy enemyPrefab;
+  [Header("Settings")]
+  [SerializeField] private int amount;
+  [SerializeField] private float radius;
+  [SerializeField] private float angle;
+
+  private void Start()
+  {
+    GenerateEnemies();
+  }
+
+  private void GenerateEnemies()
+  {
+    for (int i = 0; i < amount; i++)
+    {
+      Vector3 enemyLocalPosition = GetRunnerLocalPosition(i);
+      Vector3 enemyWorldPosition = transform.TransformPoint(enemyLocalPosition);
+
+      Instantiate(enemyPrefab, enemyWorldPosition, Quaternion.identity, transform);
+    }
+  }
+  private Vector3 GetRunnerLocalPosition(int index)
+  {
+    float x = radius * Mathf.Sqrt(index) * Mathf.Cos(Mathf.Deg2Rad * index * angle);
+    float z = radius * Mathf.Sqrt(index) * Mathf.Sin(Mathf.Deg2Rad * index * angle);
+
+    return new Vector3(x, 0, z);
+  }
+}
